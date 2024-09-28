fcf782159b1acbb932c3bc718617296f47361518
logan-tolbert <logo88205+git@gmail.com>
2024-09-28 16:57:46 -0500

    :tada: Initial commit

---
+++ Calculator/Controls/CalculatorControl.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator.Controls
+{
+    /// <summary>
+    /// Interaction logic for CalculatorControl.xaml
+    /// </summary>
+    public partial class CalculatorControl : UserControl
+    {
+        public CalculatorControl()
+        {
+            InitializeComponent();
+        }
+
+        private void acButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void positiveOrNegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void percentageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void divideButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void eightButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nineButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void multipliyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fourButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fiveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sixButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void oneButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void twoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void threeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void subtractButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zeroButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void decimalButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void equalbutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
